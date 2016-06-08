c4b0c72d24839f6c5f01f4e0f067a833f14452e9
oniwa <wasupwa@gmail.com>
2016-06-08 19:50:39 +0200

    agregando navegacion con Page.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-alert('hola')
-alert('adios')
+var page = require('page');
+
+var main = document.getElementById('main-container')
+
+page('/', function (ctx, next) {
+  main.innerHTML = 'Home<a href="/signup">signup</a>'
+})
+
+page('/signup', function (ctx, next) {
+  main.innerHTML = 'signupHome<a href="/">home</a>'
+})
+
+page()
