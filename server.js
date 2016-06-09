cbb5f3583df3d963dceb0fd3ced06f06a99a8435
oniwa <wasupwa@gmail.com>
2016-06-09 20:53:01 +0200

    cambiar titulo de la pagina

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index', { title: 'Platzigram' })
 })
 
 app.get('/signup', function (req, res) {
-  res.render('index')
+  res.render('index', { title: 'Platzigram - Signup' })
 })
 
 app.get('/signin', function (req, res) {
-  res.render('index')
+  res.render('index', { title: 'Platzigram - Signin' })
 })
 
 app.listen(3000, function (err) {
