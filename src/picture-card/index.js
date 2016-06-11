7b292d144d7ab06e465967b5e82bca2acbf1fe00
oniwa <wasupwa@gmail.com>
2016-06-11 14:09:21 +0200

    agregada la funcionalidad del like/dislike

--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,21 +1,36 @@
 var yo = require('yo-yo');
 
-module.exports = function(pic){
-  return yo`<div class="card">
-  <div class="card-image">
-    <img class="activator" src="${pic.url}">
-  </div>
-  <div class="card-content">
+module.exports = function pictureCard(pic){
+  var el
+  function render(picture){
+    return yo`<div class="card ${picture.liked ? 'liked' : ''}" >
+    <div class="card-image">
+      <img class="activator" src="${picture.url}">
+    </div>
+    <div class="card-content">
 
-    <a class="card-title" href="/user/${pic.user.username}">
-      <img src="${pic.user.avatar}" class="avatar" />
-      <span class="usename">${pic.user.username}</span>
-    </a>
-    <small class="right time">Hace un dia</small>
-    <p>
-      <a href=""><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-      <span class="left likes">${pic.likes} me gusta</span>
-    </p>
-  </div>
-</div>`
+      <a class="card-title" href="/user/${picture.user.username}">
+        <img src="${picture.user.avatar}" class="avatar" />
+        <span class="usename">${picture.user.username}</span>
+      </a>
+      <small class="right time">Hace un dia</small>
+      <p>
+        <a onclick=${like.bind(null, true)} href="#" class="left"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+        <a onclick=${like.bind(null, false)} href="#" class="left"><i class="fa fa-heart" aria-hidden="true"></i></a>
+        <span class="left likes">${picture.likes} me gusta</span>
+      </p>
+    </div>
+  </div>`
+}
+
+  function like(liked) {
+    pic.liked = liked
+    pic.likes += liked ? 1 : -1
+    var newEl = render(pic)
+    yo.update(el, newEl)
+    return false
+  }
+
+  el = render(pic)
+  return el
 }
