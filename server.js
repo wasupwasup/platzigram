5125e57256402d10baaac0efcec2c5769c9a32cf
oniwa <wasupwa@gmail.com>
2016-06-13 21:22:53 +0200

    response from server with superagent

--- server.js
+++ server.js
@@ -18,6 +18,34 @@ app.get('/signin', function (req, res) {
   res.render('index', { title: 'Platzigram - Signin' })
 })
 
+app.get('/api/pictures', function (req, res){
+  var pictures = [
+    {
+      user: {
+        username: 'miguelo',
+        avatar: 'https://pbs.twimg.com/profile_images/712613525387083776/S9cQNrx8_400x400.jpg'
+      },
+      url:'http://materializecss.com/images/office.jpg',
+      likes: 800,
+      liked: true,
+      createAt: new Date()
+    },
+    {
+      user: {
+        username: 'miguelo',
+        avatar: 'https://pbs.twimg.com/profile_images/712613525387083776/S9cQNrx8_400x400.jpg'
+      },
+      url:'http://materializecss.com/images/office.jpg',
+      likes: 0,
+      liked: false,
+      createAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ]
+  setTimeout(function(){
+    res.send(pictures)
+  }, 2000)
+})
+
 app.listen(3000, function (err) {
   if (err) return console.log('Hubo un error'), process.exit(1);
 
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,33 +2,25 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', function (ctx, next) {
+page('/', header, loadPicture, function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
-  var pictures = [
-    {
-      user: {
-        username: 'miguelo',
-        avatar: 'https://pbs.twimg.com/profile_images/712613525387083776/S9cQNrx8_400x400.jpg'
-      },
-      url:'http://materializecss.com/images/office.jpg',
-      likes: 800,
-      liked: true,
-      createAt: new Date()
-    },
-    {
-      user: {
-        username: 'miguelo',
-        avatar: 'https://pbs.twimg.com/profile_images/712613525387083776/S9cQNrx8_400x400.jpg'
-      },
-      url:'http://materializecss.com/images/office.jpg',
-      likes: 0,
-      liked: false,
-      createAt: new Date().setDate(new Date().getDate() - 10)
-    }
 
-  ]
-  empty(main).appendChild(template(pictures));
+  empty(main).appendChild(template(ctx.pictures));
 
 })
+
+function loadPicture(ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function(err, res) {
+      if (err) {
+        return console.log(err)
+      }
+      ctx.pictures = res.body
+      next()
+    })
+}
